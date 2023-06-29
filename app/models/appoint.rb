6262bed4cb8bad44d9101c119b5d61e4b67321b9
EC2 Default User <ec2-user@ip-172-31-18-45.us-east-2.compute.internal>
2023-06-29 04:13:20 +0000

    Rails課題 2回目再提出 root設定、カスタムバリデーション実装

--- app/models/appoint.rb
+++ app/models/appoint.rb
@@ -11,4 +11,11 @@ class Appoint < ApplicationRecord
     
   validates :memo,
     length: {maximum:500}
+
+  validate :period_check
+  def period_check
+    unless self.start_date.blank? and self.end_date.blank?
+      errors.add(:end_time, "は開始時刻より遅い時間を選択してください") if self.start_date > self.end_date
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => 'appoints#index'
   get 'appoints/index'
   post 'appoints/new',to: 'appoints#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
